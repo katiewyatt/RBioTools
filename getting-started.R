534ad1397631c17165842567681c0a0e30cb3f78
hanna-ford <36707713+hanna-ford@users.noreply.github.com>
2023-01-17 17:14:30 -0600

    update web guidance for RBiotools

--- getting-started.R
+++ getting-started.R
@@ -60,7 +60,7 @@
     
 # -----2.6 Install the RBiotools package -----
   
-  install.packages("RBiotools_0.5.3.tar.gz", repos = NULL, type = "source")
+  install.packages("RBiotools_0.5.4.tar.gz", repos = NULL, type = "source")
   
   
   
